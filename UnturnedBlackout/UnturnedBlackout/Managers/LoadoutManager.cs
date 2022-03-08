2571cddb62fbcc3afa48cf0cc6c542d2053cbcf1
Paradox <maazkashif492@gmail.com>
2022-03-08 20:24:03 +0500

    Made a start on the loadout management

---
+++ UnturnedBlackout/UnturnedBlackout/Managers/LoadoutManager.cs
@@ -0,0 +1,355 @@
+﻿using Rocket.Unturned.Player;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnturnedBlackout.Database.Data;
+
+namespace UnturnedBlackout.Managers
+{
+    public class LoadoutManager
+    {
+        public DatabaseManager DB { get; set; }
+        
+        public LoadoutManager()
+        {
+            DB = Plugin.Instance.DBManager;
+        }
+
+        public void EquipPrimary(UnturnedPlayer player, int loadoutID, ushort newPrimary)
+        {
+            Logging.Debug($"{player.CharacterName} is trying to switch primary to {newPrimary} for loadout with id {loadoutID}");
+            if (!DB.PlayerLoadouts.TryGetValue(player.CSteamID, out PlayerLoadout loadout))
+            {
+                Logging.Debug($"Error finding loadout for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Guns.TryGetValue(newPrimary, out LoadoutGun gun) && newPrimary != 0)
+            {
+                Logging.Debug($"Error finding loadout gun with id {newPrimary} for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Loadouts.TryGetValue(loadoutID, out Loadout playerLoadout))
+            {
+                Logging.Debug($"Error finding loadout with id {loadoutID} for {player.CharacterName}");
+                return;
+            }
+
+            playerLoadout.Primary = gun;
+            ThreadPool.QueueUserWorkItem(async (o) =>
+            {
+                await DB.UpdatePlayerLoadoutAsync(player.CSteamID, loadoutID);
+            });
+        }
+
+        public void EquipSecondary(UnturnedPlayer player, int loadoutID, ushort newSecondary)
+        {
+            Logging.Debug($"{player.CharacterName} is trying to switch secondary to {newSecondary} for loadout with id {loadoutID}");
+            if (!DB.PlayerLoadouts.TryGetValue(player.CSteamID, out PlayerLoadout loadout))
+            {
+                Logging.Debug($"Error finding loadout for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Guns.TryGetValue(newSecondary, out LoadoutGun gun) && newSecondary != 0)
+            {
+                Logging.Debug($"Error finding loadout gun with id {newSecondary} for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Loadouts.TryGetValue(loadoutID, out Loadout playerLoadout))
+            {
+                Logging.Debug($"Error finding loadout with id {loadoutID} for {player.CharacterName}");
+                return;
+            }
+
+            playerLoadout.Secondary = gun;
+            ThreadPool.QueueUserWorkItem(async (o) =>
+            {
+                await DB.UpdatePlayerLoadoutAsync(player.CSteamID, loadoutID);
+            });
+        }
+
+        public void EquipKnife(UnturnedPlayer player, int loadoutID, ushort newKnife)
+        {
+            Logging.Debug($"{player.CharacterName} is trying to switch knife to {newKnife} for loadout with id {loadoutID}");
+            if (!DB.PlayerLoadouts.TryGetValue(player.CSteamID, out PlayerLoadout loadout))
+            {
+                Logging.Debug($"Error finding loadout for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Knives.TryGetValue(newKnife, out LoadoutKnife knife) && newKnife != 0)
+            {
+                Logging.Debug($"Error finding loadout knife with id {newKnife} for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Loadouts.TryGetValue(loadoutID, out Loadout playerLoadout))
+            {
+                Logging.Debug($"Error finding loadout with id {loadoutID} for {player.CharacterName}");
+                return;
+            }
+
+            playerLoadout.Knife = knife;
+            ThreadPool.QueueUserWorkItem(async (o) =>
+            {
+                await DB.UpdatePlayerLoadoutAsync(player.CSteamID, loadoutID);
+            });
+        }
+
+        public void EquipTactical(UnturnedPlayer player, int loadoutID, ushort newTactical)
+        {
+            Logging.Debug($"{player.CharacterName} is trying to switch tactical to {newTactical} for loadout with id {loadoutID}");
+            if (!DB.PlayerLoadouts.TryGetValue(player.CSteamID, out PlayerLoadout loadout))
+            {
+                Logging.Debug($"Error finding loadout for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Gadgets.TryGetValue(newTactical, out LoadoutGadget tactical) && newTactical != 0)
+            {
+                Logging.Debug($"Error finding loadout gadget with id {newTactical} for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Loadouts.TryGetValue(loadoutID, out Loadout playerLoadout))
+            {
+                Logging.Debug($"Error finding loadout with id {loadoutID} for {player.CharacterName}");
+                return;
+            }
+
+            playerLoadout.Tactical = tactical;
+            ThreadPool.QueueUserWorkItem(async (o) =>
+            {
+                await DB.UpdatePlayerLoadoutAsync(player.CSteamID, loadoutID);
+            });
+        }
+
+        public void EquipLethal(UnturnedPlayer player, int loadoutID, ushort newLethal)
+        {
+            Logging.Debug($"{player.CharacterName} is trying to switch lethal to {newLethal} for loadout with id {loadoutID}");
+            if (!DB.PlayerLoadouts.TryGetValue(player.CSteamID, out PlayerLoadout loadout))
+            {
+                Logging.Debug($"Error finding loadout for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Gadgets.TryGetValue(newLethal, out LoadoutGadget lethal) && newLethal != 0)
+            {
+                Logging.Debug($"Error finding loadout gadget with id {newLethal} for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Loadouts.TryGetValue(loadoutID, out Loadout playerLoadout))
+            {
+                Logging.Debug($"Error finding loadout with id {loadoutID} for {player.CharacterName}");
+                return;
+            }
+
+            playerLoadout.Lethal = lethal;
+            ThreadPool.QueueUserWorkItem(async (o) =>
+            {
+                await DB.UpdatePlayerLoadoutAsync(player.CSteamID, loadoutID);
+            });
+        }
+
+        public void EquipGlove(UnturnedPlayer player, int loadoutID, ushort newGlove)
+        {
+            Logging.Debug($"{player.CharacterName} is trying to switch glove to {newGlove} for loadout with id {loadoutID}");
+            if (!DB.PlayerLoadouts.TryGetValue(player.CSteamID, out PlayerLoadout loadout))
+            {
+                Logging.Debug($"Error finding loadout for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Gloves.TryGetValue(newGlove, out LoadoutGlove glove) && newGlove != 0)
+            {
+                Logging.Debug($"Error finding loadout glove with id {newGlove} for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Loadouts.TryGetValue(loadoutID, out Loadout playerLoadout))
+            {
+                Logging.Debug($"Error finding loadout with id {loadoutID} for {player.CharacterName}");
+                return;
+            }
+
+            playerLoadout.Glove = glove;
+            ThreadPool.QueueUserWorkItem(async (o) =>
+            {
+                await DB.UpdatePlayerLoadoutAsync(player.CSteamID, loadoutID);
+            });
+        }
+
+        public void EquipCard(UnturnedPlayer player, int loadoutID, int newCard)
+        {
+            Logging.Debug($"{player.CharacterName} is trying to switch card to {newCard} for loadout with id {loadoutID}");
+            if (!DB.PlayerLoadouts.TryGetValue(player.CSteamID, out PlayerLoadout loadout))
+            {
+                Logging.Debug($"Error finding loadout for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Cards.TryGetValue(newCard, out LoadoutCard card) && newCard != 0)
+            {
+                Logging.Debug($"Error finding loadout card with id {newCard} for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Loadouts.TryGetValue(loadoutID, out Loadout playerLoadout))
+            {
+                Logging.Debug($"Error finding loadout with id {loadoutID} for {player.CharacterName}");
+                return;
+            }
+
+            playerLoadout.Card = card;
+            ThreadPool.QueueUserWorkItem(async (o) =>
+            {
+                await DB.UpdatePlayerLoadoutAsync(player.CSteamID, loadoutID);
+            });
+        }
+
+        public void EquipPerk(UnturnedPlayer player, int loadoutID, int newPerk)
+        {
+            Logging.Debug($"{player.CharacterName} is trying to equip perk with id {newPerk} for loadout with id {loadoutID}");
+            if (!DB.PlayerLoadouts.TryGetValue(player.CSteamID, out PlayerLoadout loadout))
+            {
+                Logging.Debug($"Error finding loadout for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Perks.TryGetValue(newPerk, out LoadoutPerk perk))
+            {
+                Logging.Debug($"Error finding loadout perk with id {newPerk} for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Loadouts.TryGetValue(loadoutID, out Loadout playerLoadout))
+            {
+                Logging.Debug($"Error finding loadout with id {loadoutID} for {player.CharacterName}");
+                return;
+            }
+
+            if (playerLoadout.Perks.Count == 3)
+            {
+                playerLoadout.Perks[0] = perk;
+            } else
+            {
+                playerLoadout.Perks.Add(perk);
+            }
+
+            ThreadPool.QueueUserWorkItem(async (o) =>
+            {
+                await DB.UpdatePlayerLoadoutAsync(player.CSteamID, loadoutID);
+            });
+        }
+
+        public void DequipPerk(UnturnedPlayer player, int loadoutID, int oldPerk)
+        {
+            Logging.Debug($"{player.CharacterName} is trying to dequip perk with id {oldPerk} for loadout with id {loadoutID}");
+            if (!DB.PlayerLoadouts.TryGetValue(player.CSteamID, out PlayerLoadout loadout))
+            {
+                Logging.Debug($"Error finding loadout for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Perks.TryGetValue(oldPerk, out LoadoutPerk perk))
+            {
+                Logging.Debug($"Error finding loadout perk with id {oldPerk} for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Loadouts.TryGetValue(loadoutID, out Loadout playerLoadout))
+            {
+                Logging.Debug($"Error finding loadout with id {loadoutID} for {player.CharacterName}");
+                return;
+            }
+
+            if (!playerLoadout.Perks.Remove(perk))
+            {
+                Logging.Debug($"Perk with id {oldPerk} was not equipped for {player.CharacterName} for loadout with id {loadoutID}");
+                return;
+            }
+
+            ThreadPool.QueueUserWorkItem(async (o) =>
+            {
+                await DB.UpdatePlayerLoadoutAsync(player.CSteamID, loadoutID);
+            });
+        }
+
+        public void EquipKillstreak(UnturnedPlayer player, int loadoutID, int newKillstreak)
+        {
+            Logging.Debug($"{player.CharacterName} is trying to equip killstreak with id {newKillstreak} for loadout with id {loadoutID}");
+            if (!DB.PlayerLoadouts.TryGetValue(player.CSteamID, out PlayerLoadout loadout))
+            {
+                Logging.Debug($"Error finding loadout for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Killstreaks.TryGetValue(newKillstreak, out LoadoutKillstreak killstreak))
+            {
+                Logging.Debug($"Error finding loadout killstreak with id {newKillstreak} for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Loadouts.TryGetValue(loadoutID, out Loadout playerLoadout))
+            {
+                Logging.Debug($"Error finding loadout with id {loadoutID} for {player.CharacterName}");
+                return;
+            }
+
+            if (playerLoadout.Killstreaks.Count == 3)
+            {
+                playerLoadout.Killstreaks[0] = killstreak;
+            }
+            else
+            {
+                playerLoadout.Killstreaks.Add(killstreak);
+            }
+
+            ThreadPool.QueueUserWorkItem(async (o) =>
+            {
+                await DB.UpdatePlayerLoadoutAsync(player.CSteamID, loadoutID);
+            });
+        }
+
+        public void DequipKillstreak(UnturnedPlayer player, int loadoutID, int oldKillstreak)
+        {
+            Logging.Debug($"{player.CharacterName} is trying to dequip killstreak with id {oldKillstreak} for loadout with id {loadoutID}");
+            if (!DB.PlayerLoadouts.TryGetValue(player.CSteamID, out PlayerLoadout loadout))
+            {
+                Logging.Debug($"Error finding loadout for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Killstreaks.TryGetValue(oldKillstreak, out LoadoutKillstreak killstreak))
+            {
+                Logging.Debug($"Error finding loadout killstreak with id {oldKillstreak} for {player.CharacterName}");
+                return;
+            }
+
+            if (!loadout.Loadouts.TryGetValue(loadoutID, out Loadout playerLoadout))
+            {
+                Logging.Debug($"Error finding loadout with id {loadoutID} for {player.CharacterName}");
+                return;
+            }
+
+            if (!playerLoadout.Killstreaks.Remove(killstreak))
+            {
+                Logging.Debug($"Killstreak with id {oldKillstreak} was not equipped for {player.CharacterName} for loadout with id {loadoutID}");
+                return;
+            }
+
+            ThreadPool.QueueUserWorkItem(async (o) =>
+            {
+                await DB.UpdatePlayerLoadoutAsync(player.CSteamID, loadoutID);
+            });
+        }
+    }
+}
