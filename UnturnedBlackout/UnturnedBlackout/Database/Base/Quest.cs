7e9628488ed18c32494f2270760b975ae002bc86
Paradox <maazkashif492@gmail.com>
2022-06-10 16:09:10 +0500

    c

---
+++ UnturnedBlackout/UnturnedBlackout/Database/Base/Quest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnturnedBlackout.Enums;
+
+namespace UnturnedBlackout.Database.Base
+{
+    public class Quest
+    {
+        public int QuestID { get; set; }
+        public EQuestType QuestType { get; set; }
+        public Dictionary<EQuestCondition>
+    }
+}
---
+++ UnturnedBlackout/UnturnedBlackout/Enums/EQuestCondition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnturnedBlackout.Enums
+{
+    public enum EQuestCondition
+    {
+        Gun,
+        Gadget,
+        Knife,
+        Killstreak,
+        Map,
+        Gamemode
+    }
+}
