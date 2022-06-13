e23b6ff4f39837cdf49218661297a7dcef28a249
Paradox <maazkashif492@gmail.com>
2022-06-13 20:31:37 +0500

    Changes

---
+++ UnturnedBlackout/UnturnedBlackout/Commands/WipeSpawnsCommand.cs
@@ -0,0 +1,63 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnturnedBlackout.Enums;
+
+namespace UnturnedBlackout.Commands
+{
+    class WipeSpawnsCommand : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Both;
+
+        public string Name => "wipespawns";
+
+        public string Help => "Wipe spawns";
+
+        public string Syntax => "/wipespawns (MapID) (Gamemode)";
+
+        public List<string> Aliases => new();
+
+        public List<string> Permissions => new();
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (command.Length < 2)
+            {
+                Utility.Say(caller, $"<color=red>Correct Usage: {Syntax}</color>");
+                return;
+            }
+
+            if (!int.TryParse(command[0], out int mapID))
+            {
+                Utility.Say(caller, $"<color=red>Correct Usage: {Syntax}</color>");
+                return;
+            }
+            
+            if (!Enum.TryParse(command[1], true, out EGameType gameMode))
+            {
+                Utility.Say(caller, $"<color=red>Correct Usage: {Syntax}</color>");
+                return;
+            }
+
+            switch (gameMode)
+            {
+                case EGameType.FFA:
+                    Plugin.Instance.DataManager.Data.FFASpawnPoints.RemoveAll(k => k.LocationID == mapID);
+                    break;
+                case EGameType.KC:
+                case EGameType.TDM:
+                    Plugin.Instance.DataManager.Data.TDMSpawnPoints.RemoveAll(k => k.LocationID == mapID);
+                    break;
+                case EGameType.CTF:
+                    Plugin.Instance.DataManager.Data.CTFSpawnPoints.RemoveAll(k => k.LocationID == mapID);
+                    break;
+            }
+        
+            Plugin.Instance.DataManager.SaveJson();
+            Utility.Say(caller, $"<color=green>Wiped spawnpoints for map {mapID} for gamemode {gameMode}</color>");
+        }
+    }
+}
