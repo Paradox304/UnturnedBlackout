040baf3d9cb51c5c9cdcbdb06d76a7f65fd20a88
Paradox <maazkashif492@gmail.com>
2022-09-24 19:13:27 +0500

    c

---
+++ CountryCodePopulator/CountryCodePopulator/Program.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+
+var builder = new MySqlConnectionStringBuilder()
+{
+    Server = "priv-01.modern-hosting.com",
+    Port = 3306,
+    Database = "s965_BlackoutEU",
+    UserID = "u965_JCaQjHTfJU",
+    Password = "^EF^2xWP@t+8^cSXqSsszGS7",
+    MaximumPoolSize = 500
+};
+
+var connectionString = builder.ConnectionString;
+using (var connection = new MySqlConnection(connectionString))
+{
+    connection.Open();
+    var rdr = new MySqlCommand()
+}
