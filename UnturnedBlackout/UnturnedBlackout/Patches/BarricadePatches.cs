2dc89eb1102b4c6a1a01bd58377829ee56f0fdae
Paradox <maazkashif492@gmail.com>
2022-09-29 00:43:44 +0500

    Changed destroy barricade patch

--- UnturnedBlackout/UnturnedBlackout/Patches/BarricadePatches.cs
+++ UnturnedBlackout/UnturnedBlackout/Patches/BarricadePatches.cs
@@ -4,18 +4,11 @@ using SDG.Unturned;
 namespace UnturnedBlackout.Patches;
 public class BarricadePatches
 {
-    [HarmonyPatch(typeof(BarricadeManager), nameof(BarricadeManager.ReceiveDestroyBarricade))]
+    [HarmonyPatch(typeof(BarricadeManager), nameof(BarricadeManager.destroyBarricade))]
     [HarmonyPrefix]
-    private static void BarricadeDestroyed(NetId netId)
+    private static void BarricadeDestroyed(BarricadeDrop barricade, byte x, byte y, ushort plant)
     {
-        Logging.Debug("Barricade destroy patch detected");
-        var barricade = NetIdRegistry.Get<BarricadeDrop>(netId);
-        if (barricade == null)
-        {
-            return;
-        }
-
-        Logging.Debug("Drop found, sending to all games");
+        Logging.Debug("Barricade destroy patch detected. Drop found, sending to all games");
         foreach (var game in Plugin.Instance.Game.Games)
         {
             game.OnBarricadeDestroyed(barricade);
