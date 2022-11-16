2044710c12f74eecad680721bb48ec42793a90c9
Paradox <maazkashif492@gmail.com>
2022-11-16 20:31:41 +0500

    Added /tps command
    Took 8 minutes

---
+++ UnturnedBlackout/UnturnedBlackout/Commands/TPSCommand.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Rocket.API;
+using SDG.Unturned;
+using UnturnedBlackout.Extensions;
+
+namespace UnturnedBlackout.Commands;
+
+[UsedImplicitly]
+internal class TPSCommand : IRocketCommand
+{
+    public void Execute(IRocketPlayer caller, string[] command)
+    {
+        Utility.Say(caller, $"TPS: {Provider.debugTPS}");
+        Utility.Say(caller, $"UPS: {Provider.debugUPS}");
+    }
+
+    public AllowedCaller AllowedCaller => AllowedCaller.Player;
+    public string Name => "tps";
+    public string Help => "Check the tps of the server";
+    public string Syntax => "/tps";
+    public List<string> Aliases => new();
+    public List<string> Permissions => new();
+}
