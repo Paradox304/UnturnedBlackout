1cd04ac595a25be30e2afdaec3dc1cebf1b79b69
Paradox <maazkashif492@gmail.com>
2022-12-07 20:53:23 +0500

    Changes to config
    Took 18 minutes

--- UnturnedBlackout/UnturnedBlackout/Models/Global/GameEvent.cs
+++ UnturnedBlackout/UnturnedBlackout/Models/Global/GameEvent.cs
@@ -8,16 +8,15 @@ namespace UnturnedBlackout.Models.Global;
 [Serializable]
 public class GameEvent
 {
-    public int EventID { get; set; }
-    public int EventWeight { get; set; }
     public string EventName { get; set; }
     public string EventColor { get; set; }
+    public int EventWeight { get; set; }
     
     public bool IsHardcore { get; set; }
 
-    [XmlArrayItem("LocationID")]
+    [XmlArrayItem("IgnoredLocation")]
     public List<int> IgnoredLocations { get; set; }
-    [XmlArrayItem("Gamemode")]
+    [XmlArrayItem("IgnoredGameMode")]
     public List<EGameType> IgnoredGameModes { get; set; }
     
     public GameEvent()
